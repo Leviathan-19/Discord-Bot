e46cd40260dff17aeb48ab55e1d43fe3be1d6800
Leviathan-19 <msnarvaez19@hotmail.com>
2026-02-22 11:14:43 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Bot conectado como ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!hola') {
+    message.reply('Hola 👋');
+  }
+});
+
+client.login(process.env.Token_discord);
