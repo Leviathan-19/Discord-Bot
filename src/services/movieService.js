a29b692ed68674ca5a3455dfc87c270ead38f347
Leviathan-19 <msnarvaez19@hotmail.com>
2026-02-23 17:16:11 -0500

    Add Select from movies table db in movieService.js

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,17 +1,20 @@
-const { pool } = require('../config/database');
+const db = require('../config/database');
 
-async function addMovie(title, type, year) {
-  await pool.query(
-    'INSERT INTO movies(title, type, release_year) VALUES($1,$2,$3)',
-    [title, type, year]
-  );
+async function searchTitlesByPrefix(prefix) {
+  const query = `
+    SELECT title
+    FROM movies
+    WHERE title ILIKE $1
+    ORDER BY title ASC
+    LIMIT 25
+  `;
+
+  const values = [`${prefix}%`];
+
+  const { rows } = await db.query(query, values);
+  return rows;
 }
 
-async function getRandomMovie() {
-  const result = await pool.query(
-    'SELECT * FROM movies ORDER BY RANDOM() LIMIT 1'
-  );
-  return result.rows[0];
-}
-
-module.exports = { addMovie, getRandomMovie };
+module.exports = {
+  searchTitlesByPrefix,
+};
